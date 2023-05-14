53e70588fc66dac3417b880393a097c45d05b73e
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-14 11:19:57 +0300

    qw

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 export default {
     '/test/': function() {
-        let str = '<ul>';
-
-        for (let i = 1; i < 10; i++) {
-            str += '<li>' + i + '</li>';
+        let arr = [1, 2, 3, 4, 5];
+        let show = '<div>';
+        
+        for (let elem of arr) {
+            show += '<p>' + elem + '</p>';
         }
 
-        str += '</ul>';
+        show += '</div>';
 
-        return  str;
+        return show;
     }
 }
