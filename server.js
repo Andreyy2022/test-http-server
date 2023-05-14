9166d381222f66284159ff63955fa32e2d5e9fbb
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-14 11:11:38 +0300

    servTegs

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 export default {
-	
+    '/test/': function() {
+        let str = '<ul>';
+
+        for (let i = 1; i < 10; i++) {
+            str += '<li>' + i + '</li>';
+        }
+
+        str += '</ul>';
+
+        return  str;
+    }
 }
