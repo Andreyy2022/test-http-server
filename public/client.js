7f3b68e519afecd186de73a556f941d289c170c9
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-15 12:11:13 +0300

    ajax3

--- public/client.js
+++ public/client.js
@@ -1,28 +1,42 @@
 let div = document.querySelector('div');
-let first = document.querySelector('#first');
-let second = document.querySelector('#second');
-let third = document.querySelector('#third');
+let btn = document.querySelector('button');
 
-first.addEventListener('click', function() {
+btn.addEventListener('click', function() {
     fetch('/ajax.html').then(response => {
         return response.text();
     }).then(text => {
         div.innerHTML = text;
     });
-});
 
-second.addEventListener('click', function() {
-    fetch('/ajax2.html').then(response => {
-        return response.text();
-    }).then(text => {
-        div.innerHTML = text;
+    btn.addEventListener('click', function() {
+        fetch('/ajax2.html').then(response => {
+            return response.text();
+        }).then(text => {
+            div.innerHTML = text;
+        });
+    
+        btn.addEventListener('click', function() {
+            fetch('/ajax3.html').then(response => {
+                return response.text();
+            }).then(text => {
+                div.innerHTML = text;
+            });
+        
+            btn.addEventListener('click', function() {
+                fetch('/ajax4.html').then(response => {
+                    return response.text();
+                }).then(text => {
+                    div.innerHTML = text;
+                });
+            
+                btn.addEventListener('click', function() {
+                    fetch('/ajax5.html').then(response => {
+                        return response.text();
+                    }).then(text => {
+                        div.innerHTML = text;
+                    });
+                });
+            });
+        });
     });
 });
-
-third.addEventListener('click', function() {
-    fetch('/ajax3.html').then(response => {
-        return response.text();
-    }).then(text => {
-        div.innerHTML = text;
-    });
-})
