50f6338cc08918530745db385ef3c027cd767cc7
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-16 14:06:17 +0300

    GETparam3

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 export default {
     '/handler/': function({get}) {
-        return (+get.num1 + +get.num2);
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    return arr[get.num];
     }
 }
