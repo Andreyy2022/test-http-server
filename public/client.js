156b4922907c8ba2f1bba2ad70c0a26fffd0cb40
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-16 11:18:49 +0300

    GETparam

--- public/client.js
+++ public/client.js
@@ -2,41 +2,9 @@ let div = document.querySelector('div');
 let btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
-    fetch('/ajax.html').then(response => {
+    fetch('/handler/?num=9').then(response => {
         return response.text();
     }).then(text => {
         div.innerHTML = text;
     });
-
-    btn.addEventListener('click', function() {
-        fetch('/ajax2.html').then(response => {
-            return response.text();
-        }).then(text => {
-            div.innerHTML = text;
-        });
-    
-        btn.addEventListener('click', function() {
-            fetch('/ajax3.html').then(response => {
-                return response.text();
-            }).then(text => {
-                div.innerHTML = text;
-            });
-        
-            btn.addEventListener('click', function() {
-                fetch('/ajax4.html').then(response => {
-                    return response.text();
-                }).then(text => {
-                    div.innerHTML = text;
-                });
-            
-                btn.addEventListener('click', function() {
-                    fetch('/ajax5.html').then(response => {
-                        return response.text();
-                    }).then(text => {
-                        div.innerHTML = text;
-                    });
-                }, {once: true});
-            }, {once: true});
-        }, {once: true});
-    }, {once: true});
-}, {once: true});
+});
--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 export default {
-    '/test/': function() {
-        let arr = [1, 2, 3, 4, 5];
-        let show = '<div>';
-        
-        for (let elem of arr) {
-            show += '<p>' + elem + '</p>';
-        }
-
-        show += '</div>';
-
-        return show;
+    '/handler/': function({get}) {
+        return get.num ** 2;
     }
 }
