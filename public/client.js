9ea1d6e851ba60dc86a96524e1d91e03fdb721d0
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-20 11:37:22 +0300

    GET_HTML_form

--- public/client.js
+++ public/client.js
@@ -1,10 +1,20 @@
-let div = document.querySelector('div');
-let btn = document.querySelector('button');
+let form = document.querySelector('form');
+let inp1 = document.querySelector('[name="num1"]');
+let inp2 = document.querySelector('[name="num2"]');
 
-btn.addEventListener('click', function() {
-    fetch('/handler/?num=0').then(response => {
+form.addEventListener('submit', function(event) {
+    let searchParams = new searchParams();
+
+    searchParams.set('num1', inp1.value);
+    searchParams.set('num2', inp2.value);
+
+    let path = '/handler/?' + searchParams.toString();
+
+    fetch(path).then(response => {
         return response.text();
     }).then(text => {
-        div.innerHTML = text;
+        console.log(text);
     });
+
+    event.preventDefault();
 });
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 export default {
     '/handler/': function({get}) {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    
-    return arr[get.num]; 
+        console.log(get);
+        return 'form data received';
     }
 }
